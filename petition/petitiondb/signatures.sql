320495a12626a9251e73cfb69e32265a1b814bde
Cecile Eboa <cecileeboa@gmail.com>
2021-01-22 09:57:18 +0100

    connected to db

---
+++ petition/petitiondb/signatures.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS signatures;
+
+ CREATE TABLE signatures (
+     id SERIAL PRIMARY KEY,
+     first VARCHAR NOT NULL CHECK (first != ''),
+     last VARCHAR NOT NULL CHECK (last != ''),
+     signature VARCHAR NOT NULL CHECK (signature != '')
+ );
+
